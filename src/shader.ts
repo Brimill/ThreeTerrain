9960d190f0c8d7a50af5dcabb0cc649ca80a7442
Rüdiger Erdweg <r.erdweg@gmail.com>
2025-08-31 23:00:19 +0200

    🚧 WIP fragment shader

---
+++ src/shader.ts
@@ -0,0 +1,17 @@
+import { shaderMaterial } from "@react-three/drei";
+import { extend } from "@react-three/fiber";
+
+export const GroundMaterial = shaderMaterial(
+  {},
+  // vertex shader
+  `
+    varying vec2 vUv;
+    void main(){
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }`,
+  // fragment shader
+  `void main(){
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}`
+);
