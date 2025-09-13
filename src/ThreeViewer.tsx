cf66a605e715d9889107efe1ce47383b7a8b7df6
Rüdiger Erdweg <r.erdweg@gmail.com>
2025-09-12 22:02:02 -0400

    ➕ Add r3f-perf as fps counter

--- src/ThreeViewer.tsx
+++ src/ThreeViewer.tsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
 import Scene from "./Scene";
 
 function ThreeViewer() {
@@ -6,6 +7,7 @@ function ThreeViewer() {
     <div id="canvas-container" className="size-full bg-muted">
       <Canvas>
         <Scene />
+        <Perf position="bottom-right" deepAnalyze={true} minimal={false} />
       </Canvas>
     </div>
   );
