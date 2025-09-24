b7385cd90df1c6c09e1b8b2b90acc417d0d3d1b5
Rüdiger Erdweg <r.erdweg@gmail.com>
2025-09-25 00:08:35 +0200

    ✨ Add option to switch to second scene

---
+++ src/Scene2.tsx
@@ -0,0 +1,61 @@
+import { useThree } from "@react-three/fiber";
+import Ground from "./Ground";
+import { PerspectiveCamera as PerspectiveCameraType } from "three";
+import { PerspectiveCamera } from "@react-three/drei";
+import { useAppStore } from "./stores";
+import { useEffect, useMemo, useRef } from "react";
+import { useControls } from "leva";
+import GroundCPU from "./GroundCPU";
+import { TerrainGenerator } from "./TerrainGenerator";
+
+function Scene2() {
+  const { camera, size: viewportSize } = useThree();
+  const { useGPU } = useControls(
+    "Settings",
+    { useGPU: false },
+    { collapsed: true },
+  );
+  const size: number = useAppStore((state) => state.size);
+  const frequencies: number[] = useAppStore((state) => state.frequencies);
+  const amplitudes: number[] = useAppStore((state) => state.amplitudes);
+  const cameraRef = useRef<PerspectiveCameraType>(null);
+  const cameraHeight = size * 1.5;
+  const terrainGenerator = useMemo(
+    () => new TerrainGenerator(frequencies, amplitudes),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  useEffect(() => {
+    if (!cameraRef.current) return;
+    cameraRef.current.aspect = viewportSize.width / viewportSize.height;
+    cameraRef.current.up.set(0, 0, 1); // Set Z as up direction
+    cameraRef.current.updateProjectionMatrix();
+  }, [viewportSize, camera]);
+
+  useEffect(() => {
+    console.log("useEffect: size or cameraHeight changed", size, cameraHeight);
+    if (cameraRef.current) {
+      cameraRef.current.lookAt(0, 0, 0);
+    }
+  }, [size, cameraHeight]);
+
+  return (
+    <>
+      <axesHelper args={[size]} />
+      <ambientLight intensity={0.5} />
+      <PerspectiveCamera
+        makeDefault
+        ref={cameraRef}
+        position={[-size, -size, size / 2]} // X, Y, Z (Z is up)
+      />
+      {useGPU ? (
+        <Ground terrainGenerator={terrainGenerator} />
+      ) : (
+        <GroundCPU terrainGenerator={terrainGenerator} />
+      )}
+    </>
+  );
+}
+
+export default Scene2;
--- src/ThreeViewer.tsx
+++ src/ThreeViewer.tsx
@@ -1,15 +1,34 @@
 import { Canvas } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 import Scene from "./Scene";
+import { ToggleGroup, ToggleGroupItem } from "./components/ui/toggle-group";
+import { useState } from "react";
+import Scene2 from "./Scene2";
 
 function ThreeViewer() {
+  const [showScene1, setShowScene1] = useState<boolean>(true);
   return (
-    <div id="canvas-container" className="size-full bg-muted">
-      <Canvas>
-        <Scene />
-        <Perf position="bottom-right" deepAnalyze={true} minimal={true} />
-      </Canvas>
-    </div>
+    <>
+      <div id="canvas-container" className="size-full bg-muted flex flex-col">
+        <Canvas>
+          {showScene1 ? <Scene /> : <Scene2 />}
+          <Perf position="bottom-right" deepAnalyze={true} minimal={true} />
+        </Canvas>
+        <ToggleGroup
+          variant="outline"
+          type="single"
+          defaultValue="scene1"
+          value={showScene1 ? "scene1" : "scene2"}
+          className={"self-center z-50 bg-white dark:bg-black"}
+          onValueChange={(value) => {
+            setShowScene1(value === "scene1");
+          }}
+        >
+          <ToggleGroupItem value="scene1">Scene 1</ToggleGroupItem>
+          <ToggleGroupItem value="scene2">Scene 2</ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+    </>
   );
 }
 
